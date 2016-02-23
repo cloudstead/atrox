65da865476e5edce991fe8184331f63754beb7e8
jonathan <jonathan@kyuss.org>
2016-02-23 10:19:08 -0800

    make some arguments optional

--- histori-server/src/main/java/histori/main/wiki/ArticleNexusOptions.java
+++ histori-server/src/main/java/histori/main/wiki/ArticleNexusOptions.java
@@ -14,10 +14,10 @@ public class ArticleNexusOptions extends WikiBaseOptions {
     @Option(name=OPT_FILE, aliases=LONGOPT_FILE, usage=USAGE_FILE)
     @Getter @Setter private String input = null;
 
-    public static final String USAGE_OUTPUT_DIR = "Output directory";
+    public static final String USAGE_OUTPUT_DIR = "Output directory. Default is to print to stdout.";
     public static final String OPT_OUTPUT_DIR = "-o";
     public static final String LONGOPT_OUTPUT_DIR= "--output-dir";
-    @Option(name=OPT_OUTPUT_DIR, aliases=LONGOPT_OUTPUT_DIR, usage=USAGE_OUTPUT_DIR, required=true)
+    @Option(name=OPT_OUTPUT_DIR, aliases=LONGOPT_OUTPUT_DIR, usage=USAGE_OUTPUT_DIR)
     @Getter @Setter private File outputDir;
 
     public static final String USAGE_OVERWRITE = "Overwrite output files. Default is to preserve files.";
@@ -29,7 +29,7 @@ public class ArticleNexusOptions extends WikiBaseOptions {
     public static final String USAGE_ERROR_LOG = "Error log. Articles that cannot be parsed will be written here, one per line.";
     public static final String OPT_ERROR_LOG = "-E";
     public static final String LONGOPT_ERROR_LOG= "--error-log";
-    @Option(name=OPT_ERROR_LOG, aliases=LONGOPT_ERROR_LOG, usage=USAGE_ERROR_LOG, required=true)
+    @Option(name=OPT_ERROR_LOG, aliases=LONGOPT_ERROR_LOG, usage=USAGE_ERROR_LOG)
     @Getter @Setter private File errorLog;
     public boolean hasErrorLog () { return errorLog != null; }
 
