06660ddae1fe8f4d7b92d0f67a46387f75b85a51
jonathan <jonathan@kyuss.org>
2016-03-01 18:09:37 -0800

    add color picker for marker icons

--- histori-server/src/main/resources/site/js/histori/search.js
+++ histori-server/src/main/resources/site/js/histori/search.js
@@ -4,10 +4,37 @@ function closeSearchOptions () { closeForm('searchOptionsContainer'); }
 MARKER_COLORS = ['blue','brown','darkgreen','green','orange','paleblue','pink','purple','red','yellow'];
 
 function initSearchForm () {
+
     addSearchRow(false);
+
     var picker = $('#markerColorPickerContainer');
     for (var i=0; i<MARKER_COLORS.length; i++) {
-        picker.append('<img src="markers/'+MARKER_COLORS[i]+'_Marker_blank.png"/>');
+        var templateImage = $('<img src="markers/'+MARKER_COLORS[i]+'_Marker_blank.png"/>');
+        var color = MARKER_COLORS[i];
+        templateImage.click(colorPickerClickHandler(color));
+        picker.append(templateImage);
+    }
+}
+
+function colorPickerClickHandler (color) {
+    return function (e) {
+        var id = $('#colorPickerRowId').val();
+
+        var markerId = 'marker_' + id;
+        var markerImage = $('#'+markerId);
+
+        var textId = 'text_' + id;
+        var searchBox = $('#' + textId);
+        var initial = getInitialFromSearchBox(searchBox);
+
+        var currentSrc = markerImage.attr('src');
+        var newMarkerImage = 'markers/'+color+'_Marker'+initial+'.png';
+
+        // only replace src if it is different
+        if (currentSrc.indexOf(newMarkerImage) == -1) {
+            markerImage.attr('src', newMarkerImage);
+        }
+        closeMarkerColorPicker();
     }
 }
 
@@ -51,18 +78,25 @@ function addSearchRow (includeRemoveIcon) {
     tbody.append(row);
 }
 
-function openMarkerColorPicker (e) {
+function openMarkerColorPicker (e, id) {
 
-    var activeColor = getColorFromImage(img.src);
-    console.log('clicked on a '+activeColor+' marker');
+    var markerId = 'marker_' + id;
+    var markerImage = $('#'+markerId);
+
+    var textId = 'text_' + id;
+
+    var currentSrc = markerImage.attr('src');
+    var currentColor = getColorFromImage(currentSrc);
+
+    console.log('clicked on a '+currentColor+' marker');
+    $('#colorPickerRowId').val(id); // store id in picker
     var picker = $('#markerColorPickerContainer');
-    var pos = $(img).position();
-    var width = $(img).outerWidth();
+    var width = 0;
     picker.css({
         zIndex: 1,
         position: "absolute",
-        top: pos.top + "px",
-        left: (pos.left + width) + "px"
+        top: e.pageY + "px",
+        left: (e.pageX + width) + "px"
     });
 }
 
@@ -74,6 +108,36 @@ function closeMarkerColorPicker () {
 // strip leading prefix before first underscore, the remainder is the id for the row
 function searchRowIdFromOtherId(id) { return id.substring(id.indexOf('_') + 1); }
 
+function getInitialFromSearchBox(searchBox) {
+    var searchTerms = searchBox.val();
+    var initial = '_blank';
+    if (typeof searchTerms != "undefined" && searchTerms != null && searchTerms.length > 0) {
+        for (var i = 0; i < searchTerms.length; i++) {
+            if (searchTerms.charAt(i).match(/[a-z]/i)) {
+                initial = searchTerms.charAt(i).toUpperCase();
+                break;
+            }
+        }
+    }
+    return initial;
+}
+function updateMarkerInitialLetter(id) {
+    var markerId = 'marker_' + id;
+    var markerImage = $('#' + markerId);
+
+    var textId = 'text_' + id;
+    var searchBox = $('#' + textId);
+
+    var currentSrc = markerImage.attr('src');
+    var color = getColorFromImage(currentSrc);
+    var initial = getInitialFromSearchBox(searchBox);
+    var newMarkerImage = 'markers/' + color + '_Marker' + initial + '.png';
+
+    // only replace src if it is different
+    if (currentSrc.indexOf(newMarkerImage) == -1) {
+        markerImage.attr('src', newMarkerImage);
+    }
+}
 function buildSearchRow (color, includeRemoveIcon) {
 
     var id = guid();
@@ -83,11 +147,13 @@ function buildSearchRow (color, includeRemoveIcon) {
     var markerCell = $('<td id="markerClickTarget_'+id+'" align="center" valign="middle"></td>').append(markerImage);
 
     markerImage.click(function (e) {
+        var id = searchRowIdFromOtherId(e.target.id);
         var image = $('#'+e.target.id);
         var parent = image.parent();
         var childImages = parent.find('.searchBox_markerImage');
         var src = childImages[0].src;
         color = getColorFromImage(src);
+        openMarkerColorPicker(e, id);
         console.log('marker clicked, target=' + e.target.id + ', color='+color);
     });
     row.append(markerCell);
@@ -95,32 +161,7 @@ function buildSearchRow (color, includeRemoveIcon) {
     var queryTextField = $('<input id="text_'+id+'" class="searchBox_query" type="text"/>');
     queryTextField.keyup(function (e) {
         var id = searchRowIdFromOtherId(e.target.id);
-
-        var markerId = 'marker_' + id;
-        var markerImage = $('#'+markerId);
-
-        var textId = 'text_' + id;
-        var searchBox = $('#'+textId);
-
-        var currentSrc = markerImage.attr('src');
-        var color = getColorFromImage(currentSrc);
-
-        var searchTerms = searchBox.val();
-        var initial = '_blank';
-        if (typeof searchTerms != "undefined" && searchTerms != null && searchTerms.length > 0) {
-            for (var i=0; i<searchTerms.length; i++) {
-                if (searchTerms.charAt(i).match(/[a-z]/i)) {
-                    initial = searchTerms.charAt(i).toUpperCase();
-                    break;
-                }
-            }
-        }
-        var newMarkerImage = 'markers/'+color+'_Marker'+initial+'.png';
-
-        // only replace src if it is different
-        if (currentSrc.indexOf(newMarkerImage) == -1) {
-            markerImage.attr('src', newMarkerImage);
-        }
+        updateMarkerInitialLetter(id);
     });
 
     var queryCell = $('<td align="center"></td>').append(queryTextField);
@@ -140,4 +181,3 @@ function buildSearchRow (color, includeRemoveIcon) {
 
     return row;
 }
-
