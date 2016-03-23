e1c80b913e27f3a3ba2077b97429425544dc77fe
jonathan <jonathan@kyuss.org>
2016-03-22 17:51:02 -0700

    tweak java_opts

--- histori-server/chef-repo/cookbooks/histori/attributes/default.rb
+++ histori-server/chef-repo/cookbooks/histori/attributes/default.rb
@@ -4,4 +4,4 @@ default[:histori][:service_name] = 'histori-api-server.HistoriServer' # don't ch
 default[:histori][:dbuser] = 'histori'
 default[:histori][:master_db] = 'histori_master'
 default[:histori][:db_prefix] = 'histori_'
-default[:histori][:java_opts] = '-Xmx1800m -Xmx1600m'
+default[:histori][:java_opts] = '-Xmx1800m -Xms600m'
