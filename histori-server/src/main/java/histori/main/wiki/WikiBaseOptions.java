bc1b91984feda6952f18de1dacfa6f54979e74dd
jonathan <jonathan@kyuss.org>
2016-03-28 13:14:02 -0700

    improve help/error detection when using s3 storage

--- histori-server/src/main/java/histori/main/wiki/WikiBaseOptions.java
+++ histori-server/src/main/java/histori/main/wiki/WikiBaseOptions.java
@@ -15,6 +15,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import static org.cobbzilla.util.daemon.ZillaRuntime.die;
+import static org.cobbzilla.util.daemon.ZillaRuntime.empty;
 import static org.cobbzilla.util.io.FileUtil.abs;
 
 public class WikiBaseOptions extends BaseMainOptions {
@@ -25,7 +26,10 @@ public class WikiBaseOptions extends BaseMainOptions {
     @Option(name=OPT_PLACES_API_KEY, aliases=LONGOPT_PLACES_API_KEY, usage=USAGE_PLACES_API_KEY)
     @Getter @Setter private String placesApiKey;
 
-    public static final String USAGE_WIKI_DIR = "Base directory for wiki archive. Format may be filesystem path or s3:bucket/prefix/path/ (use WIKI_S3_ACCESS and WIKI_S3_SECRET env vars to set credentials)";
+    public static final String WIKI_S3_ACCESS = "WIKI_S3_ACCESS";
+    public static final String WIKI_S3_SECRET = "WIKI_S3_SECRET";
+
+    public static final String USAGE_WIKI_DIR = "Base directory for wiki archive. Format may be filesystem path or s3:bucket/prefix/path/ (use "+WIKI_S3_ACCESS+" and "+WIKI_S3_SECRET+" env vars to set credentials)";
     public static final String OPT_WIKI_DIR = "-w";
     public static final String LONGOPT_WIKI_DIR= "--wiki-dir";
     @Option(name=OPT_WIKI_DIR, aliases=LONGOPT_WIKI_DIR, usage=USAGE_WIKI_DIR, required=true)
@@ -46,8 +50,13 @@ public class WikiBaseOptions extends BaseMainOptions {
         if (wikiDir.startsWith("s3:")) {
             final Matcher matcher = s3pathPattern.matcher(wikiDir);
             if (!matcher.find()) die("invalid s3 path:"+wikiDir);
-            config.put(S3AssetStorageService.PROP_ACCESS_KEY, System.getenv("WIKI_S3_ACCESS"));
-            config.put(S3AssetStorageService.PROP_SECRET_KEY, System.getenv("WIKI_S3_SECRET"));
+
+            final String access = System.getenv(WIKI_S3_ACCESS);
+            final String secret = System.getenv(WIKI_S3_SECRET);
+            if (empty(access) || empty(secret)) die("env var not found: "+WIKI_S3_ACCESS + " and/or "+WIKI_S3_SECRET);
+
+            config.put(S3AssetStorageService.PROP_ACCESS_KEY, access);
+            config.put(S3AssetStorageService.PROP_SECRET_KEY, secret);
             config.put(S3AssetStorageService.PROP_BUCKET, matcher.group(1));
             config.put(S3AssetStorageService.PROP_PREFIX, matcher.group(2));
             config.put(S3AssetStorageService.PROP_LOCAL_CACHE, System.getenv("WIKI_S3_LOCAL_CACHE"));
