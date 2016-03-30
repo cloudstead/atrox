ec2c0756ca44a67c2723d5c480ec36b1848aaa82
jonathan <jonathan@kyuss.org>
2016-03-30 14:57:16 -0700

    increase line log interval

--- histori-server/src/main/java/histori/main/wiki/WikiIndexerMain.java
+++ histori-server/src/main/java/histori/main/wiki/WikiIndexerMain.java
@@ -64,6 +64,8 @@ public class WikiIndexerMain extends MainBase<WikiIndexerOptions> {
     public static final String TEXT_TAG_OPEN = "<text ";
     public static final String TEXT_TAG_CLOSE = "</text>";
 
+    public static final int LINELOG_INTERVAL = 1_000_000;
+
     public static void main (String[] args) { main(WikiIndexerMain.class, args); }
 
     private int pageCount = 0;
@@ -104,9 +106,9 @@ public class WikiIndexerMain extends MainBase<WikiIndexerOptions> {
                     }
                 }
                 if (lineCount <= skipLines) {
-                    if (lineCount % 100000 == 0) out("skipped line "+lineCount+"/"+skipLines);
+                    if (lineCount % LINELOG_INTERVAL == 0) out("skipped line "+lineCount+"/"+skipLines);
                     continue;
-                } else if (stopLines > 0 && lineCount % 100000 == 0) {
+                } else if (stopLines > 0 && lineCount % LINELOG_INTERVAL == 0) {
                     out("processed line "+(lineCount-skipLines)+"/"+stopLines +" : "+(100.0*((double)(lineCount-skipLines))/((double)stopLines))+" % done");
                 }
 
