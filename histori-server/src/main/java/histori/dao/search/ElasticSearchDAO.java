b1c9fa329e6fbd07ad590ef4ad22d7e78339dab9
jonathan <jonathan@kyuss.org>
2016-04-05 13:07:03 -0700

    update search interface

--- histori-server/src/main/java/histori/dao/search/ElasticSearchDAO.java
+++ histori-server/src/main/java/histori/dao/search/ElasticSearchDAO.java
@@ -9,7 +9,7 @@ import histori.server.HistoriConfiguration;
 import lombok.extern.slf4j.Slf4j;
 import org.cobbzilla.wizard.dao.AbstractElasticSearchDAO;
 import org.cobbzilla.wizard.server.config.ElasticSearchConfig;
-import org.elasticsearch.action.search.SearchRequestBuilder;
+import org.elasticsearch.index.query.QueryBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -52,18 +52,9 @@ public class ElasticSearchDAO extends AbstractElasticSearchDAO<Nexus, SearchQuer
         return NexusSummary.comparator(searchQuery.getSummarySortOrder());
     }
 
-    @Override protected SearchRequestBuilder prepareSearch(SearchQuery searchQuery, SearchRequestBuilder searchRequestBuilder) {
-        final TimeRange range = searchQuery.getTimeRange();
-        final long startInstant = range.getStartPoint().getDateInstant();
-        final long endInstant = range.getEndPoint().getDateInstant();
-        final GeoBounds bounds = searchQuery.getBounds();
-        double north = bounds.getNorth();
-        double south = bounds.getSouth();
-        double east = bounds.getEast();
-        double west = bounds.getWest();
+    @Override protected QueryBuilder getQuery(SearchQuery searchQuery) {
         final String query = trimQuotes(searchQuery.getQuery());
-
-        return searchRequestBuilder.setQuery(boolQuery().must(boolQuery()
+        return boolQuery().must(boolQuery()
                 .should(commonTermsQuery("name", query).boost(1.5f))
                 .should(commonTermsQuery("nexusType", query).boost(2.5f))
                 .should(commonTermsQuery("canonicalName", query).boost(2.0f))
@@ -73,24 +64,34 @@ public class ElasticSearchDAO extends AbstractElasticSearchDAO<Nexus, SearchQuer
                 .should(commonTermsQuery("tags.values.field", query).boost(0.8f))
                 .should(commonTermsQuery("tags.values.value", query).boost(0.8f))
                 .should(commonTermsQuery("markdown", query).boost(0.1f))
-        ))
-                // filters are time + geo
-                .setPostFilter(boolQuery()
-                        // either entity start or end must fall within query time range
-                        .must(boolQuery()
-                                .should(rangeQuery("timeRange.startPoint.dateInstant").from(startInstant).to(endInstant))
-                                .should(rangeQuery("timeRange.endPoint.dateInstant").from(startInstant).to(endInstant)))
-
-                        // one of the corners of the bounding rectangle must be within the bounds of the query
-                        // there are more complex polygon-overlap algorithms but would be much harder to implement against
-                        // general-purpose tech like a search engine
-                        // todo: consider: a custom geo-optimized storage engine
-                        .must(boolQuery()
-                                .should(geoBoundingBoxQuery("bounds.topLeft").topLeft(north, west).bottomRight(south, east))
-                                .should(geoBoundingBoxQuery("bounds.topRight").topLeft(north, west).bottomRight(south, east))
-                                .should(geoBoundingBoxQuery("bounds.bottomLeft").topLeft(north, west).bottomRight(south, east))
-                                .should(geoBoundingBoxQuery("bounds.bottomRight").topLeft(north, west).bottomRight(south, east)))
-                );
+        );
     }
 
+    @Override protected QueryBuilder getPostFilter(SearchQuery searchQuery) {
+        final TimeRange range = searchQuery.getTimeRange();
+        final long startInstant = range.getStartPoint().getDateInstant();
+        final long endInstant = range.getEndPoint().getDateInstant();
+        final GeoBounds bounds = searchQuery.getBounds();
+        double north = bounds.getNorth();
+        double south = bounds.getSouth();
+        double east = bounds.getEast();
+        double west = bounds.getWest();
+        return boolQuery()
+                // either entity start or end must fall within query time range
+                .must(boolQuery()
+                        .should(rangeQuery("timeRange.startPoint.dateInstant").from(startInstant).to(endInstant))
+                        .should(rangeQuery("timeRange.endPoint.dateInstant").from(startInstant).to(endInstant))
+                )
+
+                // one of the corners of the bounding rectangle must be within the bounds of the query
+                // there are more complex polygon-overlap algorithms but would be much harder to implement against
+                // general-purpose tech like a search engine
+                // todo: consider: a custom geo-optimized storage engine
+                .must(boolQuery()
+                        .should(geoBoundingBoxQuery("bounds.topLeft").topLeft(north, west).bottomRight(south, east))
+                        .should(geoBoundingBoxQuery("bounds.topRight").topLeft(north, west).bottomRight(south, east))
+                        .should(geoBoundingBoxQuery("bounds.bottomLeft").topLeft(north, west).bottomRight(south, east))
+                        .should(geoBoundingBoxQuery("bounds.bottomRight").topLeft(north, west).bottomRight(south, east))
+                );
+    }
 }
