e328297882269f1fe8d2e35b09857ed3264db3f5
jonathan <jonathan@kyuss.org>
2016-04-27 10:42:42 -0700

    we need postgresql 9.4 support

--- histori-server/chef-repo/cookbooks/postgresql/recipes/default.rb
+++ histori-server/chef-repo/cookbooks/postgresql/recipes/default.rb
@@ -3,15 +3,17 @@
 # Recipe:: default
 #
 
-%w( postgresql-9.3 ).each do |pkg|
-  package pkg do
-    action :install
-  end
-end
-
 bash 'ensure postgresql is running' do
   user 'root'
   code <<-EOF
+
+if [ ! -x /usr/bin/psql ] ; then
+  echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list
+  wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
+  apt-get update
+  apt-get install postgresql-9.4
+fi
+
 status="$(service postgresql status)"
 if [ -z "${status}" ] ; then
   echo "Error getting postgresql service status"
