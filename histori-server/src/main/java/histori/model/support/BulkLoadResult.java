434eeaa13290fa8c880af22133621b5e7b103f51
jonathan <jonathan@kyuss.org>
2016-04-26 18:16:26 -0700

    initialize successes

--- histori-server/src/main/java/histori/model/support/BulkLoadResult.java
+++ histori-server/src/main/java/histori/model/support/BulkLoadResult.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 import org.cobbzilla.wizard.validation.ValidationResult;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.cobbzilla.util.daemon.ZillaRuntime.now;
@@ -15,7 +16,7 @@ public class BulkLoadResult {
     @Getter @Setter private boolean completed;
     @Getter @Setter private boolean cancelled = false;
     @Getter @Setter private Exception exception;
-    @Getter @Setter private List<String> successes;
+    @Getter @Setter private List<String> successes = new ArrayList<>();
     @Getter private ValidationResult validation = new ValidationResult();
 
     @JsonIgnore public long getAge () { return now() - startTime; }
