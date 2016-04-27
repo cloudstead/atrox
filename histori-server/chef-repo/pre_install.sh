3fc2b3e41014a79ff9edba4ad9137b5151896163
jonathan <jonathan@kyuss.org>
2016-04-27 10:53:04 -0700

    allow site content to be downloaded

--- histori-server/chef-repo/pre_install.sh
+++ histori-server/chef-repo/pre_install.sh
@@ -9,6 +9,15 @@ if [[ $(whoami) != "root" ]] ; then
   die "Must run as root"
 fi
 
+function download {
+  dest=${1}
+  url=${2}
+  if [ ! -f ${dest} ] ; then
+    curl ${url} > ${dest} || die "Error downloading ${url} -> ${dest}"
+    chown ${CHEF_USER} ${dest} || die "Error running chown ${CHEF_USER} ${dest}"
+  fi
+}
+
 CHEF_USER=$(cat /etc/chef-user)
 if [ -z ${CHEF_USER} ] ; then
   die "No chef user found in /etc/chef-user"
@@ -18,7 +27,8 @@ CHEF_USER_HOME=$(bash -c "cd ~${CHEF_USER} && pwd")
 JAR="${CHEF_USER_HOME}/chef/cookbooks/histori/files/default/assets/histori-server.jar"
 JAR_URL="http://kyuss.org/downloads/histori-server.jar"
 
-if [ ! -f ${JAR} ] ; then
-  curl ${JAR_URL} > ${JAR} || die "Error downloading ${JAR_URL} -> ${JAR}"
-  chown ${CHEF_USER} ${JAR} || die "Error running chown ${CHEF_USER} ${JAR}"
-fi
+SITE="${CHEF_USER_HOME}/chef/cookbooks/histori/files/default/assets/histori-site.jar"
+SITE_URL="http://kyuss.org/downloads/histori-site.jar"
+
+download ${JAR} ${JAR_URL}
+download ${SITE} ${SITE_URL}
